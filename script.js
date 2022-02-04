974793e0b0bf393ea209af7508be52630c548ab0
Gacho2021 <87105994+Gacho2021@users.noreply.github.com>
2022-02-03 19:02:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let scoreCount = -1;       // Set counter to 1
+let scoreTotal = -999;
+let entry;    // Message
+ 
+// enter scores until 999 is entered
+while (entry != 999) {
+  entry = parseInt(prompt('Enter test score\nOr enter 999 to end entries'));
+  scoreCount++;
+  scoreTotal += entry;
+}
+ 
+alert('Average score is ' + scoreTotal / scoreCount);
 
